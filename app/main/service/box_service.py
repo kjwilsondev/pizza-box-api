8db2369aa8eba956f5f2dcae74b1ebad0b20a32a
jaebooker <jaesonbooker@gmail.com>
2019-12-07 17:21:56 -0800

    made changes to services

--- app/main/service/box_service.py
+++ app/main/service/box_service.py
@@ -14,30 +14,13 @@ def save_new_box(data):
             registered_on=datetime.datetime.utcnow()
         )
         save_changes(new_box)
-        return generate_token(new_box)
+        return new_box
     else:
         response_object = {
             'status': 'fail',
-            'message': 'Box already exists. Please Log in.',
+            'message': 'An error occurred',
         }
         return response_object, 409
-
-def generate_token(box):
-    try:
-        # generate the auth token
-        auth_token = user.encode_auth_token(box.id)
-        response_object = {
-            'status': 'success',
-            'message': 'Successfully registered.',
-            'Authorization': auth_token.decode()
-        }
-        return response_object, 201
-    except Exception as e:
-        response_object = {
-            'status': 'fail',
-            'message': 'Some error occurred. Please try again.'
-        }
-        return response_object, 401
 
 def get_all_boxes():
     return Box.query.all()
