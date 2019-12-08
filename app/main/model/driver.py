cbfacb0b4887961272c944d8ab63779d55f28e7f
TasfiaAddrita <tasfia.addrita@gmail.com>
2019-12-07 16:41:02 -0800

    [add] driver model

---
+++ app/main/model/driver.py
@@ -0,0 +1,79 @@
+from .. import db, flask_bcrypt
+import datetime
+from app.main.model.blacklist import BlacklistToken
+from ..config import key
+import jwt
+from box import Box
+
+# Driver class inherits from db.Model class which declares the class as a model for sqlalchemy
+class Driver(db.Model):
+    """ Driver Model for storing Driver related details """
+    __tablename__ = "Driver"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    registered_on = db.Column(db.DateTime, nullable=False)
+    admin = db.Column(db.Boolean, nullable=False, default=False)
+    public_id = db.Column(db.String(100), unique=True)
+    username = db.Column(db.String(50), unique=True)
+    password_hash = db.Column(db.String(100))
+    restaurant = db.Column(db.String(100))
+    destination = db.Column(db.String(100))
+    num_boxes = db.Column(db.Integer, default=0)
+    completion = db.Column(db.Boolean, default=False)
+
+    @property
+    def password(self):
+        raise AttributeError('password: write-only field')
+
+    # password.setter uses flask-bcrypt to generate a hash using the provided password.
+    @password.setter
+    def password(self, password):
+        self.password_hash = flask_bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def check_password(self, password):
+        return flask_bcrypt.check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return "<Driver '{}'>".format(self.Drivername)
+
+    @staticmethod
+    def encode_auth_token(Driver_id):
+        """
+        Generates the Auth Token
+        :return: string
+        """
+        try:
+            payload = {
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(days=1, seconds=5),
+                'iat': datetime.datetime.utcnow(),
+                'sub': Driver_id
+            }
+            return jwt.encode(
+                payload,
+                key,
+                algorithm='HS256'
+            )
+        except Exception as e:
+            return e
+
+    @staticmethod
+    def decode_auth_token(auth_token):
+        """
+        Decodes the auth token
+        :param auth_token:
+        :return: integer|string
+        """
+        try:
+            payload = jwt.decode(auth_token, key)
+            is_blacklisted_token = BlacklistToken.check_blacklist(auth_token)
+            if is_blacklisted_token:
+                return 'Token blacklisted. Please log in again.'
+            else:
+                # returns Driver_id
+                return payload['sub']
+        except jwt.ExpiredSignatureError:
+            return 'Signature expired. Please log in again.'
+        except jwt.InvalidTokenError:
+            return 'Invalid token. Please log in again.'
+            
